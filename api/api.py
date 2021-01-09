16dc5e13ae30e67b664a6153c3bb93fb1bb0ced0
Musa Mohannad <musa.mohannad1@gmail.com>
2021-01-09 16:47:03 -0700

    sessions lol

--- api/api.py
+++ api/api.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, \
+url_for, flash, make_response, session
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super secret'
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World!'
+
+@app.route('/visits-counter/')
+def visits():
+    if 'visits' in session:
+        # Reading and updating session data
+        session['visits'] = session.get('visits') + 1
+    else:
+        # Setting session data
+        session['visits'] = 1
+    return "Total visits: {}".format(session.get('visits'))
 
 
+if __name__ == '__main__':
+    app.run()
