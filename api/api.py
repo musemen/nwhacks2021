d6a6a59e664de73912f905f4236a800973557c0c
Samuel Kahessay <samuelkahessay@gmail.com>
2021-01-09 23:59:04 -0700

    updates

--- api/api.py
+++ api/api.py
@@ -3,6 +3,29 @@ url_for, flash, make_response, session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret'
+<<<<<<< Updated upstream
+=======
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+CORS(app)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Question = db.Column(db.String(40), nullable=False)
+    Background = db.Column(db.String(200), nullable=False)
+    Resources = db.Column(db.String(60), nullable=False)
+    prompts = db.Column(db.String(60), nullable=False)
+    # Import from other prompts table
+    
+
+    def __repr__(self):
+        return f"Question('{self.Question}','{self.Background}')"
+
+class Prompts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prompt = db.Column(db.String(60), nullable=False)
+
+>>>>>>> Stashed changes
 
 @app.route('/')
 def hello_world():
