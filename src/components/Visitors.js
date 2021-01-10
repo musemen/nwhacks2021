8b923df8419e1d9ea20b2031e4f7290d33c48413
Musa Mohannad <musa.mohannad1@gmail.com>
2021-01-09 19:19:21 -0700

    frontend query from backend

---
+++ src/components/Visitors.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+function Visitors() {
+
+  const [currentuser, setCurrentUser] = useState(0);
+
+  useEffect(() => {
+    fetch('/visits-counter/').then(res => res.json()).then(data => {
+      setCurrentUser(data.Visits);
+    });
+  }, []);
+
+    return (
+        <p>The unique number of visitors is {currentuser}.</p>
+    );
+  }
+
+  export default Visitors;
