d86264ba58ce8cd6e8ee8f97e7e028bd4afef6df
Musa Mohannad <musa.mohannad1@gmail.com>
2021-01-09 17:58:13 -0700

    query backend from frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './css/App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 import Home from './components/Home';
 import Vote from './components/Vote';
+import Visitors from './components/Visitors';
 import Results from './components/Results';
 import CreateQuestion from './components/CreateQuestion';
 
 function App() {
+
   return (
     <div className="App">
         <Router>
             <Route exact path="/" render={() => <Home/>}/>
+            <Route exact path="/visitors" render={() => <Visitors/>}/>
             <Route exact path="/vote" render={() => <Vote/>}/>
             <Route exact path="/results" render={() => <Results/>}/>
             <Route exact path="/create-question" render={() => <CreateQuestion/>}/>
