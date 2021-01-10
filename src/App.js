9c7465e03a4b753aa972bce190f0bb05c3acf4ef
song <songeun.you7@gmail.com>
2021-01-09 17:36:09 -0700

    Add basic question cards to home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './css/App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './components/Home';
 import Vote from './components/Vote';
@@ -11,6 +11,15 @@ function App() {
   return (
     <div className="App">
         <Router>
+            <div id="navbar">
+                <Link to="/">
+                    <div className="home">Home</div>
+                </Link>
+                <Link to="/create-question">
+                    <div className="submit-q">Submit A Question</div>
+                </Link>
+            </div>
+
             <Route exact path="/" render={() => <Home/>}/>
             <Route exact path="/vote" render={() => <Vote/>}/>
             <Route exact path="/results" render={() => <Results/>}/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,40 @@
+import { Link } from 'react-router-dom';
+import '../css/Home.css';
+
+let questions = [
+    {
+        question: "What do you want to do tomorrow?",
+        id: 1
+    },
+    {
+        question: "New project name?",
+        id: 2
+    },
+    {
+        question: "New project name?",
+        id: 3
+    }
+];
+
+function QuestionCard(props) {
+  return (
+    <Link to={`/vote/${props.id}`}>
+        <div className="card">
+            <h1>{props.question}</h1>
+        </div>
+    </Link>
+  );
+}
+
 function Home() {
   return (
-    <div className="home">
-        <h1>HOME</h1>
+    <div id="home">
+        <h1>Answer questions</h1>
+
+        {questions.map(q =>
+            <QuestionCard id={q.id} question={q.question}/>
+        )}
+
     </div>
   );
 }
